158d114827505d7cfde119591680e47ef8456407
Justin Hynes <JHynes@edx.org>
2022-01-05 07:55:47 -0500

    chore: bump notices version after Python updates
    - Bump notices plugin version to 0.14.1

--- notices/__init__.py
+++ notices/__init__.py
@@ -2,6 +2,6 @@
 An edx-platform plugin which manages notices that must be acknowledged.
 """
 
-__version__ = "0.14.0"
+__version__ = "0.14.1"
 
 default_app_config = "notices.apps.NoticesConfig"  # pylint: disable=invalid-name
